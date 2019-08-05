465db7a53836c20c59b12addf98a300d4f75b8c6
xingchen1624 <xingchen19890624@163.com>
2019-08-05 22:40:32 +0800

    第一次初始化提交

---
+++ jpaTest1/src/main/java/com/jpa/domain/Customer.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (C), 2012-2019, by xavier chen
+ * FileName: Customer
+ * Author:   chen
+ * Date:     2019/8/5 22:15
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jpa.domain;
+
+import javax.persistence.*;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试spring data jpa使用的实体类〉
+ *
+ * @author chen
+ * @createDate 2019/8/5
+ * @since 1.0.0
+ *
+ * @Entity:声明该类是一个实体类
+ * @Table:配置实体类和表的映射关系
+ */
+@Entity
+@Table(name = "cst_customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //主键生成策略
+    @Column(name = "cust_id")
+    private Long id;
+
+    @Column(name = "cust_name")
+    private String custName;
+
+    @Column(name = "cust_source")
+    private String custSource;
+
+    @Column(name = "cust_level")
+    private String custLevel;
+
+    @Column(name = "cust_industry")
+    private String custIndustry;
+
+    @Column(name = "cust_phone")
+    private String custPhone;
+
+    @Column(name = "cust_address")
+    private String custAddress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public String getCustSource() {
+        return custSource;
+    }
+
+    public void setCustSource(String custSource) {
+        this.custSource = custSource;
+    }
+
+    public String getCustLevel() {
+        return custLevel;
+    }
+
+    public void setCustLevel(String custLevel) {
+        this.custLevel = custLevel;
+    }
+
+    public String getCustIndustry() {
+        return custIndustry;
+    }
+
+    public void setCustIndustry(String custIndustry) {
+        this.custIndustry = custIndustry;
+    }
+
+    public String getCustPhone() {
+        return custPhone;
+    }
+
+    public void setCustPhone(String custPhone) {
+        this.custPhone = custPhone;
+    }
+
+    public String getCustAddress() {
+        return custAddress;
+    }
+
+    public void setCustAddress(String custAddress) {
+        this.custAddress = custAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", custName='" + custName + '\'' +
+                ", custSource='" + custSource + '\'' +
+                ", custLevel='" + custLevel + '\'' +
+                ", custIndustry='" + custIndustry + '\'' +
+                ", custPhone='" + custPhone + '\'' +
+                ", custAddress='" + custAddress + '\'' +
+                '}';
+    }
+}
---
+++ jpaTest1/src/test/java/com/jpa/test/JpaTest.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C), 2012-2019, by xavier chen
+ * FileName: JpaTest
+ * Author:   chen
+ * Date:     2019/8/5 22:33
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jpa.test;
+
+import org.junit.Test;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author chen
+ * @createDate 2019/8/5
+ * @since 1.0.0
+ */
+public class JpaTest {
+
+    /**
+     * 功能描述: <br>
+     *     使用jpa测试保存功能
+     * @Author lzc
+     * @Description
+     * @Date 22:34 2019/8/5
+     * @Param []
+     * @return void
+     **/
+    @Test
+    public void testSave(){
+        //1.加载配置文件创建工厂（实体管理类工厂）
+        //2.通过实体管理类工厂获取实体管理器
+        //3.获取事务对象，开启事务
+        //4.完成增删改查操作
+        //5.事务提交或回滚
+        //6.释放相应资源
+    }
+}
