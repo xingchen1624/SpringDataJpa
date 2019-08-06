f57a07ff40b7971e1f8bae8de0a76ca155f96502
xingchen1624 <xingchen19890624@163.com>
2019-08-06 20:13:30 +0800

    临时提交，切换到develop分支开发
    临时提交，切换到develop分支继续开发，master分支作为稳定版本使用

--- jpaTest1/src/test/java/com/jpa/test/JpaTest.java
+++ jpaTest1/src/test/java/com/jpa/test/JpaTest.java
@@ -10,34 +10,58 @@
  */
 package com.jpa.test;
 
+import com.jpa.domain.Customer;
 import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 /**
  * 〈一句话功能简述〉<br> 
- * 〈〉
+ * 〈jpa测试类〉
  *
  * @author chen
  * @createDate 2019/8/5
  * @since 1.0.0
  */
 public class JpaTest {
+    EntityManagerFactory entityManagerFactory = null;
+    EntityTransaction transaction = null;
+    EntityManager entityManager = null;
 
     /**
      * 功能描述: <br>
      *     使用jpa测试保存功能
-     * @Author lzc
-     * @Description
-     * @Date 22:34 2019/8/5
-     * @Param []
-     * @return void
      **/
     @Test
     public void testSave(){
-        //1.加载配置文件创建工厂（实体管理类工厂）
-        //2.通过实体管理类工厂获取实体管理器
-        //3.获取事务对象，开启事务
-        //4.完成增删改查操作
-        //5.事务提交或回滚
-        //6.释放相应资源
+        try {
+            //1.加载配置文件创建工厂（实体管理类工厂）
+            entityManagerFactory = Persistence.createEntityManagerFactory("myJpa");
+            //2.通过实体管理类工厂获取实体管理器
+            entityManager = entityManagerFactory.createEntityManager();
+            //3.获取事务对象，开启事务
+            transaction = entityManager.getTransaction();
+            transaction.begin(); //开启事务
+            //4.完成增删改查操作
+            Customer customer = new Customer();
+            customer.setCustName("传智播客");
+            customer.setCustPhone("10080");
+            customer.setCustAddress("江苏省常州市金坛区");
+            entityManager.persist(customer); //保存
+            //5.事务提交或回滚
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            transaction.rollback();
+        } finally {
+            //6.释放相应资源
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+
     }
 }
